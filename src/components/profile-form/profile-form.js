bfbf2edcfbba04d9ca286ff4ec337a81a3980486
SofyaSum <55841528+SofyaSum@users.noreply.github.com>
2022-02-03 18:28:33 +0700

    Add 5

---
+++ src/components/profile-form/profile-form.js
@@ -0,0 +1,57 @@
+import { TextField, Button } from "@mui/material";
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { updateProfile } from "../../store/profile";
+import styles from "./profile-form.module.css";
+
+export function ProfileForm({ firstName, lastName, phone }) {
+  const [form, setForm] = useState({ firstName, lastName, phone });
+  const dispatch = useDispatch();
+
+  const handleChangeForm = (e) => {
+    const field = e.target.getAttribute("data-name");
+
+    setForm({
+      ...form,
+      [field]: e.target.value,
+    });
+  };
+
+  return (
+    <div className={styles.wrapper}>
+      <h1>Edit profile</h1>
+
+      <div className={styles.form}>
+        <TextField 
+          fullWidth
+          placeholder="Введите first name..."
+          value={form.firstName}
+          inputProps={{
+            "data-name": "firstName",
+          }}
+          onChange={handleChangeForm}
+        />
+        <TextField 
+          fullWidth
+          placeholder="Введите last name..."
+          value={form.lastName}
+          inputProps={{
+            "data-name": "lastName",
+          }}
+          onChange={handleChangeForm}
+        />
+        <TextField 
+          fullWidth
+          placeholder="Введите  phone..."
+          value={form.phone}
+          inputProps={{
+            "data-name": "phone",
+          }}
+          onChange={handleChangeForm}
+        />
+
+        <Button variant="outlined" onClick={() => dispatch(updateProfile(form))}>save form</Button>
+      </div>
+    </div>
+  );
+}
